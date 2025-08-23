ef2e8e0f6adadda568e0752ae59581e582533de7
Kareem-Abosalem <kareemabosalem15@gmail.com>
2025-08-23 20:25:37 +0300

    Create precompute_divisors.cpp

---
+++ problem_solving/math/divisors/precompute_divisors.cpp
@@ -0,0 +1,12 @@
+const int N=1e5; // Don't forget to resize N
+vector<int>divs[N+1];
+void pre() // Don't forget to call pre  in main
+{
+    
+    for (int i = 1; i <=N; i++)
+    {
+        for (int j = i; j <=N; j+=i)
+        {
+            divs[j].push_back(i);
+        }   
+    }   
