18cbaf7feae2edda28d27a21fa8e2d0ad7a95ac1
Kareem-Abosalem <kareemabosalem15@gmail.com>
2025-08-23 20:56:54 +0300

    Create prime_factorization.cpp

---
+++ problem_solving/math/Prime_Factorization/prime_factorization.cpp
@@ -0,0 +1,15 @@
+vector<int>prime_factorization(int n)
+{
+    vector<int>res;
+    for (int i = 2; i * i <= n; i++)
+    {
+        while (n%i==0)
+        {
+            res.push_back(i);
+            n/=i;
+        }   
+    }
+    if(n>1)
+    res.push_back(n);
+    return res;
+}
