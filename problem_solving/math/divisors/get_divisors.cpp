c423d8243dde9257165b6829ed77f1722aa0767e
Kareem-Abosalem <kareemabosalem15@gmail.com>
2025-08-23 20:21:04 +0300

    Create get_divisors.cpp

---
+++ problem_solving/math/divisors/get_divisors.cpp
@@ -0,0 +1,16 @@
+vector<int> get_divisors(int n)
+{
+    vector<int>divisors;
+    for (int i = 1; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            divisors.push_back(i);
+            if (i * i != n)
+            {
+                divisors.push_back(n / i);
+            }
+        }
+    }
+    return divisors;
+}
